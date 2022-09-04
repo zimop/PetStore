21541b6287d4a352edf0c11bb218d1e21c4c0d14
zefzheng <zefzheng@student.unimelb.edu.au>
2022-09-05 01:29:34 +1000

    Add rough Recommendations Card to itemPage

--- react-frontend/src/containers/pages/ItemPage/threeTabs/recommendationCard/recommendationCard.js
+++ react-frontend/src/containers/pages/ItemPage/threeTabs/recommendationCard/recommendationCard.js
@@ -3,48 +3,113 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
 
 import "./recommendationCard.css";
 
 export default function RecommendationCard() {
   return (
     <Card sx={{ minWidth: 275 }}>
-      {/* <CardContent>
-        <div className="review-header">
-          <Avatar
-            sx={{ bgcolor: "avatarColor.deepOrange" }}
-            alt="Remy Sharp"
-            src="/broken-image.jpg"
-          >
-            O
-          </Avatar>
+      <CardContent>
+        <Container maxWidth="lg">
+          <Grid container spacing={5} margin-bottom={5}>
+            {/* First Card */}
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>
+                <Link herf="/">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      src="https://picsum.photos/200/200"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Add To Cart</Button>
+                      <Button size="small">View More</Button>
+                    </CardActions>
+                  </Card>
+                </Link>
+              </Box>
+            </Grid>
 
-          <Avatar src="/broken-image.jpg" />
+            {/* Second Card */}
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>
+                <Link herf="/">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      src="https://picsum.photos/200/201"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Add To Cart</Button>
+                      <Button size="small">View More</Button>
+                    </CardActions>
+                  </Card>
+                </Link>
+              </Box>
+            </Grid>
 
-          <Typography
-            variant="h5"
-            sx={{ fontWeight: "bold" }}
-            component="div"
-            marginLeft={1}
-            marginRight={1}
-          >
-            Omar Sundaram
-          </Typography>
-
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            9 hours ago
-          </Typography>
-        </div>
-
-        <Typography variant="body2">
-          <br />
-          {
-            "The point of using Lorem lpsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. The point of using Lorem lpsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. The point of using Lorem lpsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-          }
-        </Typography>
-      </CardContent> */}
+            {/* Third Card */}
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>
+                <Link herf="/">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      src="https://picsum.photos/200/202"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Add To Cart</Button>
+                      <Button size="small">View More</Button>
+                    </CardActions>
+                  </Card>
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </CardContent>
 
       <CardActions>
         <Button size="small" right>
