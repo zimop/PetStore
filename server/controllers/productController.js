5b76bfdf7799bacc73d269d8bcd70a4eefe8aebe
Wenzheng Du <dwz99999@gmail.com>
2022-09-18 15:56:38 +1000

    Integrate database

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,14 +1,9 @@
+productModel = require("../models/productModel.js")
 
-
-const getAllProducts = (req, res) => {
-    let placeholderObject = {
-        productId: 0,
-        productImage: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=640",
-        productName: "Dog food",
-        price: 7.99
-    }
-    let placeholderData = Array(10).fill(placeholderObject);
-    res.json(placeholderData);
+const getAllProducts = async (req, res) => {
+    let data = await productModel.getProductList();
+    console.log(data);
+    res.json(data);
 }
 
 module.exports = {
