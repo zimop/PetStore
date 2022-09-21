dc74a0733e610d8c663b21cf2e0ce6e81962c504
Wenzheng Du <dwz99999@gmail.com>
2022-09-21 22:41:19 +1000

    format code with prettier

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.json()) 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const productRouter = require("./routers/productRouter");
 
@@ -15,18 +15,18 @@ apiRouter.use(productRouter);
 
 app.use("/api", apiRouter);
 
-
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../react-frontend/build')));
+app.use(express.static(path.resolve(__dirname, "../react-frontend/build")));
 
 app.use("/api", apiRouter);
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../react-frontend/build', 'index.html'));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../react-frontend/build", "index.html")
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
--- server/mysqlHandle.js
+++ server/mysqlHandle.js
@@ -3,24 +3,24 @@ const secrets = require("../secrets.json");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "petstore.cfb1caugxl5o.ap-southeast-2.rds.amazonaws.com",
-    user: "app",
-    password: secrets.dbPassword,
-    database: "petstore"
-})
+  host: "petstore.cfb1caugxl5o.ap-southeast-2.rds.amazonaws.com",
+  user: "app",
+  password: secrets.dbPassword,
+  database: "petstore",
+});
 
 const query = (sql) => {
-    return new Promise((resolve, reject) => {
-        connection.query(sql, (error, results, fields) => {
-            if (error) {
-                return reject(error);
-            }
-            resolve(results);
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
+};
 
 module.exports = {
-    connection,
-    query,
+  connection,
+  query,
 };
