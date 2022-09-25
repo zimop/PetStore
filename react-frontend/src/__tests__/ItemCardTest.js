005c07d023806d945ce816aa157292b7d76e360c
Wenzheng Du <dwz99999@gmail.com>
2022-09-25 13:08:32 +1000

    added react tests for rendering itemcard

---
+++ react-frontend/src/__tests__/ItemCardTest.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ItemCard from "../containers/components/ItemCard";
+
+const itemData = {
+  ProductId: 3,
+  ProductName: "Morbius",
+  Price: 1000000000.0,
+  ProductImage: "https://picsum.photos/200/200",
+};
+
+test("Test rendering", () => {
+  render(<ItemCard itemData={itemData} />);
+  const title = screen.getByText("Morbius");
+  expect(title).toBeInTheDocument();
+  const price = screen.getByText("$1000000000.00");
+  expect(price).toBeInTheDocument();
+});
