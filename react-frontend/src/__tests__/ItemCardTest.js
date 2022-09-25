db0dbd7e7e53502c6ddb3ae74e1e7fb6cced0682
Wenzheng Du <dwz99999@gmail.com>
2022-09-25 13:24:20 +1000

    Remove unused imports

--- react-frontend/src/__tests__/ItemCardTest.js
+++ react-frontend/src/__tests__/ItemCardTest.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import ItemCard from "../containers/components/ItemCard";
 
 const itemData = {
