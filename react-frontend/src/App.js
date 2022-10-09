96399074252ef862957ef71bebeb2199ccd4feec
Wenzheng Du <dwz99999@gmail.com>
2022-10-09 21:54:13 +1100

    Add sign up page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -8,6 +8,7 @@ import ItemPage from "./containers/pages/ItemPage/itemPage";
 import Catalogue from "./containers/pages/Catalogue/catalogue";
 import HomePage from "./containers/pages/homePage/homePage";
 import LoginPage from "./containers/pages/Login/loginPage";
+import SignUpPage from "./containers/pages/Login/signUpPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/catalogue" element={<Catalogue />} />
           <Route path="/product/:productId" element={<ItemPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
---
+++ react-frontend/src/containers/pages/Login/signUpPage.js
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Typography,
+  FormControl,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
+
+import "./loginPage.css";
+
+const SignUpPage = (props) => {
+  return (
+    // Adapted from https://github.com/mui/material-ui/blob/v5.10.8/docs/data/material/getting-started/templates/sign-in/SignIn.js
+    // Licenced under MIT licence
+    <div className="loginContainer">
+      <Typography variant="h5">Sign up for Petshop</Typography>
+      <FormControl className="loginForm">
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="firstname"
+          label="First Name"
+          name="firstname"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="lastname"
+          label="Last Name"
+          name="lastname"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="confirm-password"
+          label="Confirm Password"
+          type="confirm-password"
+          id="confirm-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Link href="#" variant="body2">
+          {"Already have an account? Login here"}
+        </Link>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign Up
+        </Button>
+      </FormControl>
+    </div>
+  );
+};
+
+export default SignUpPage;
