0b2b7fa89e6ebf561a18c44678c371d949eb220e
zefzheng <zefzheng@student.unimelb.edu.au>
2022-10-11 13:28:28 +1100

    Add text in clickCollect

--- react-frontend/src/containers/pages/homePage/staticPage/clickCollectPage.js
+++ react-frontend/src/containers/pages/homePage/staticPage/clickCollectPage.js
@@ -6,25 +6,100 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import AddTaskIcon from "@mui/icons-material/AddTask";
 
 const ClickCollectPage = (props) => {
   return (
     <div classname="clickCollectBackground">
-      <Card sx={{ maxWidth: 800, my: 20, mx: "auto" }}>
+      <Card sx={{ maxWidth: 800, mt: 20, mb: 5, mx: "auto" }}>
         <CardContent>
-          <p>First card</p>
-          {/* Flex box */}
-          {/* https://www.petbarn.com.au/click-and-collect */}
-          {/* https://lottiefiles.com/978-walking-deer ???*/}
+          <Typography
+            gutterBottom
+            variant="h4"
+            fontWeight={"bold"}
+            component="div"
+          >
+            How Click & Collect works?
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"20%"}
+          >
+            <AttachFileIcon sx={{ mr: 1 }} />
+            Shop online and find a product.
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"20%"}
+          >
+            <AttachFileIcon sx={{ mr: 1 }} />
+            Select the product and check it is available for pick up in-store.
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"20%"}
+          >
+            <AttachFileIcon sx={{ mr: 1 }} />
+            Check stock availability in your local store.
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"20%"}
+          >
+            <AttachFileIcon sx={{ mr: 1 }} />
+            Add the product to your shopping cart and checkout
+          </Typography>
         </CardContent>
       </Card>
 
-      <Card sx={{ maxWidth: 800, my: 20, mx: "auto" }}>
+      <Card sx={{ maxWidth: 800, my: 5, mx: "auto" }}>
         <CardContent>
-          <p>Second card</p>
-          {/* Flex box */}
-          {/* https://www.petbarn.com.au/click-and-collect */}
-          {/* https://lottiefiles.com/978-walking-deer ???*/}
+          <Typography
+            gutterBottom
+            variant="h4"
+            fontWeight={"bold"}
+            component="div"
+          >
+            E-mail confirmation & pick up
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"8%"}
+          >
+            <AddTaskIcon sx={{ mr: 1 }} />
+            We’ll send an email when your order is ready for collection.
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"8%"}
+          >
+            <AddTaskIcon sx={{ mr: 1 }} />
+            Remember to bring your order confirmation, in either printed or
+            digital form, as well as personal valid identification
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            textAlign={"left"}
+            marginLeft={"8%"}
+          >
+            <AddTaskIcon sx={{ mr: 1 }} />
+            In-store please proceed to the check-out counter where a staff
+            member will process your order.
+          </Typography>
         </CardContent>
       </Card>
     </div>
