ed337cf461e2121d9514f3d5f639cb9a9858b07d
andjpritchard <100889171+andjpritchard@users.noreply.github.com>
2022-10-13 01:29:41 +1100

    Made basic display of product row with name, stock, and edit and delete buttons

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,6 +7,7 @@ import AppLayout from "./containers/layout/index";
 import ItemPage from "./containers/pages/ItemPage/itemPage";
 import Catalogue from "./containers/pages/Catalogue/catalogue";
 import HomePage from "./containers/pages/homePage/homePage";
+import ManagerBrowse from "./containers/pages/ManagerBrowse/managerBrowse";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/catalogue" element={<Catalogue />} />
           <Route path="/product/:productId" element={<ItemPage />} />
+          <Route path="/manager/browse" element={<ManagerBrowse />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
