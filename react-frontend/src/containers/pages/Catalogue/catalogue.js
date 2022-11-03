5a7780cbee567daf8731f556d02234598f77d374
zefzheng <zefzheng@student.unimelb.edu.au>
2022-11-03 22:55:36 +1100

    Add Sort Select and arrange their positions

--- react-frontend/src/containers/pages/Catalogue/catalogue.js
+++ react-frontend/src/containers/pages/Catalogue/catalogue.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-import SearchIcon from "@mui/icons-material/Search";
-
+import SearchBar from "../../components/SearchBar/SearchBar";
 import ItemCard from "../../components/ItemCard";
 
 import "./catalogue.css";
+// import  { Search, SearchIconWrapper, StyledInputBase } from "../../components/SearchBar/SearchBar.js";
 
 class Catalogue extends React.Component {
   constructor(props) {
@@ -45,18 +47,35 @@ class Catalogue extends React.Component {
       <div style={{ padding: "5%" }}>
         <div className="catalogue-header">
           {/* Header */}
-          <Typography variant="h1">Catalogue</Typography>
+          <Typography variant="h2">Catalogue</Typography>
 
-          {/* Search Bar */}
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <div className="search-sort-wrapper">
+            {/* Search Bar */}
+            <SearchBar />
+
+            <div>
+              {/* <SortIcon> */}
+              <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">
+                  Sort
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                  // onChange={handleChange}
+                  autoWidth
+                  label="Sort"
+                >
+                  <MenuItem value="">
+                    <em>Default</em>
+                  </MenuItem>
+                  <MenuItem value={0}>Price Ascending</MenuItem>
+                  <MenuItem value={1}>Price Descending</MenuItem>
+                </Select>
+              </FormControl>
+              {/* </SortIcon> */}
+            </div>
+          </div>
         </div>
 
         <Grid container spacing={4}>
@@ -73,46 +92,46 @@ const getCatalogueData = async () => {
   return data;
 };
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-  border: "1px solid",
-  borderColor: theme.palette.primary.main,
-}));
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginRight: theme.spacing(2),
+//   marginLeft: 0,
+//   width: "100%",
+//   [theme.breakpoints.up("sm")]: {
+//     marginLeft: theme.spacing(3),
+//     width: "auto",
+//   },
+//   border: "1px solid",
+//   borderColor: theme.palette.primary.main,
+// }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "inherit",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     width: "100%",
+//     [theme.breakpoints.up("md")]: {
+//       width: "20ch",
+//     },
+//   },
+// }));
 
 export default Catalogue;
