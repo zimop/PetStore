8d56f4d53f67509e67f55ea5e502b5aa6d883950
zefzheng <zefzheng@student.unimelb.edu.au>
2022-11-07 16:22:20 +1100

    Rewrite basic function for productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,8 @@
 productModel = require("../models/productModel.js");
 imageModel = require("../models/imageModel.js");
 
-const getAllProducts = async (req, res) => {
-  let data = await productModel.getProductList();
-  data = await Promise.all(
+const getProductsImages = async (data) => {
+  return await Promise.all(
     data.map(async (row) => {
       row.ProductImage = await imageModel.getFirstImageByProductID(
         row.ProductId
@@ -11,8 +10,33 @@ const getAllProducts = async (req, res) => {
       return row;
     })
   );
+};
+
+const getAllProducts = async (req, res) => {
+  let data = await productModel.getProductList();
+  data = getProductsImages(data);
   res.json(data);
 };
+
+const getCatProducts = async (req, res) => {
+  try {
+    let data = await productModel.getCatProducts();
+    if (!data) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    data = getProductsImages(data);
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getDogProducts = async (req, res) => {};
+const getMouseProducts = async (req, res) => {};
+const getRabbitProducts = async (req, res) => {};
+const getBirdProducts = async (req, res) => {};
+const getFishProducts = async (req, res) => {};
 
 const getProductById = async (req, res) => {
   try {
@@ -32,5 +56,11 @@ const getProductById = async (req, res) => {
 
 module.exports = {
   getAllProducts,
+  getCatsProducts,
+  getDogsProducts,
+  getMouseProducts,
+  getRabbitsProducts,
+  getBirdsProducts,
+  getFishProducts,
   getProductById,
 };
